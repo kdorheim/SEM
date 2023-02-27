8d532fb64180a61b2aaec9d5a1ede6b6c7176bcd
kdorheim <kalyn.dorheim@pnnl.gov>
2023-02-26 21:08:45 -0500

    save working

--- tests/testthat/test-style.R
+++
@@ -1,6 +0,0 @@
-# if (requireNamespace("lintr", quietly = TRUE)) {
-#   context("Package style")
-#   test_that("No lints found", {
-#     lintr::expect_lint_free(exclusions = list("tests/", "data-raw"))
-#   })
-# }
